67cd9172ba12c5708cf3410812b8b706991030cb
djanfarou <bilalidjanfarou@gmail.com>
2026-05-25 19:43:01 +0000

    Mise a jour du code et ajout de l'interface admin

--- tourism/models.py
+++ tourism/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
 # Create your models here.
+class TouristSite(models.Model):
+    name = models.CharField(max_length=200)
+    description = models.TextField()
+    location = models.CharField(max_length=255)
+    image = models.ImageField(upload_to='site/')
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.name
--- tourism/views.py
+++ tourism/views.py
@@ -1,4 +1,12 @@
 from django.shortcuts import render
+from .models import TouristSite
+
 
 def home(request):
+    sites = TouristSite.objects.all()
+
+    context = {
+        'sites' : sites
+    }
+
     return render(request, 'tourism/index.html')
