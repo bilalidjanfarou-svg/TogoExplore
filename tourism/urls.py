00de7f8a1f556bf8f5a93a148c8b15fce1c21e14
djanfarou <bilalidjanfarou@gmail.com>
2026-05-28 18:53:37 +0000

    Affichage dynamique des données, Création des pages dynamiques, Amélioration du design avec Bootstrap, Création d’une page Contact, Mise en place de la recherche

--- tourism/urls.py
+++ tourism/urls.py
@@ -3,4 +3,8 @@ from . import views
 
 urlpatterns = [
     path('', views.home, name='home'),
+
+    path('site/<int:id>/', views.site_detail, name='site_detail'),
+
+    path('contact/', views.contact, name='contact'),
 ]
--- tourism/views.py
+++ tourism/views.py
@@ -1,12 +1,32 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from .models import TouristSite
 
 
 def home(request):
-    sites = TouristSite.objects.all()
+
+    query = request.Get.get('q')
+
+    if query:
+        sites = TouristeSite.objects.filter(name__icontains=query)
+    else:
+        sites = TouristeSite.object.all()
 
     context = {
-        'sites' : sites
+        'sites': sites
     }
 
-    return render(request, 'tourism/index.html')
+    return render(request, 'tourism/index.html', context)
+
+def site_detail(request, id):
+     
+     site = get_object_or_404(TouristSite, id=id)
+
+    context = {
+        'site': site
+    }
+
+    return render(request, 'tourism/site_detail.html', context)
+
+def contact(request):
+
+    return render(request, 'tourism/contact.html')
