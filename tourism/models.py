ce624bbc54fad97d0b23bf560a398c7c1fd7b600
djanfarou <bilalidjanfarou@gmail.com>
2026-05-31 17:30:52 +0000

    Ajout des catégories, filtres, recherche et préparation de la géolocalisation

--- tourism/models.py
+++ tourism/models.py
@@ -1,12 +1,30 @@
 from django.db import models
 
 # Create your models here.
+
+class Category(models.Model):
+    name = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.name
+        
 class TouristSite(models.Model):
     name = models.CharField(max_length=200)
     description = models.TextField()
     location = models.CharField(max_length=255)
     image = models.ImageField(upload_to='site/')
+    category = models.ForeignKey(
+        Category,
+        on_delete=models.CASCADE,
+        null=True,
+        blank=True
+    )
+
+    latitude = models.FloatField(null=True, blank=True)
+    longitude = models.FloatField(null=True, blank=True)
+
     created_at = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
         return self.name
+
--- tourism/views.py
+++ tourism/views.py
@@ -1,31 +1,40 @@
 from django.shortcuts import render, get_object_or_404
-from .models import TouristSite
+from .models import TouristSite, Category
 
 
 def home(request):
 
-    query = request.Get.get('q')
+    query = request.GET.get('q')
+    category_id = request.GET.get('category')
+
+    sites = TouristSite.objects.all()
 
     if query:
-        sites = TouristeSite.objects.filter(name__icontains=query)
-    else:
-        sites = TouristeSite.object.all()
+        sites = sites.filter(name__icontains=query)
+
+    if category_id:
+        sites = sites.filter(category_id=category_id)
+
+    categories = Category.objects.all()
 
     context = {
-        'sites': sites
+        'sites': sites,
+        'categories': categories
     }
 
     return render(request, 'tourism/index.html', context)
 
+
 def site_detail(request, id):
-     
-     site = get_object_or_404(TouristSite, id=id)
+
+    site = get_object_or_404(TouristSite, id=id)
 
     context = {
         'site': site
     }
 
     return render(request, 'tourism/site_detail.html', context)
+
 
 def contact(request):
 
